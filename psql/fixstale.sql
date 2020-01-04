67e0f1817a14f573d00af7cb156cbd0ed357ec33
HapyM3al <tianholmes58@gmail.com>
2020-01-04 19:05:15 +0200

    Delete fixstale.sql

--- psql/fixstale.sql
+++
@@ -1,1 +0,0 @@
-update radacct set acctstoptime = acctstarttime + (acctsessiontime || 'second')::interval where acctstoptime is null  and acctstarttime + (acctsessiontime+1800 || 'second')::interval < now();
